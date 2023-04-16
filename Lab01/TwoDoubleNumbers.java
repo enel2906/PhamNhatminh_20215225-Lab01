5aaed6b246ce8ebe44a84187822b0cbc103d4465
enel2906 <117955652+enel2906@users.noreply.github.com>
2023-04-16 22:34:02 +0700

    Update TwoDoubleNumbers.java

--- Lab01/TwoDoubleNumbers.java
+++ Lab01/TwoDoubleNumbers.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-
-
-public class TwoDoubleNumbers {
+public class ShowTwoNumbers {
       public static void main(String[]args){
-            String strNum1, strNum2;
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter the first number: ");
-            strNum1 = sc.nextLine();
-            System.out.print("Enter the second number: ");
-            strNum2 = sc.next();
+            String strNum1, strNum2, strNotification = "You're just entered: ";
+
+            strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
+
+            strNotification += strNum1 + " and ";
+
+            strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ","Input the second number", JOptionPane.INFORMATION_MESSAGE);
+
             
+            strNotification += strNum2;
+
+            JOptionPane.showMessageDialog(null, strNotification, "Show two number",JOptionPane.INFORMATION_MESSAGE);
+
             double num1 = Double.parseDouble(strNum1);
             double num2 = Double.parseDouble(strNum2);
 
-            System.out.println("The sum of "+num1+" and "+num2+" is: "+sum(num1, num2));
-            System.out.println("The difference of "+num1+" with "+num2+" is: "+difference(num1, num2));
-            System.out.println("The product of "+num1+" and "+num2+" is: "+product(num1, num2));
-            if(num2 == 0) System.out.println("The divisor is invalid!!!");
-            else{
-                  System.out.println("The quotient of "+num1+" with "+num2+" is: "+quotient(num1, num2));
-            }
+            JOptionPane.showMessageDialog(null, num1 + " + "+num2 +" = "+sum(num1, num2),"SUM OF TWO NUMBERS",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, num1+" - "+num2+" = "+difference(num1, num2), "THE DIFFERENCE OF TWO NUMBER", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, num1+" * "+num2+" = "+product(num1, num2),"THE PRODUCT OF TWO NUMBER", JOptionPane.INFORMATION_MESSAGE);
+            if(num2 == 0) JOptionPane.showMessageDialog(null, "CAN CALCULATE THE QUOTIENT BECAUSE THE DIVISOR IS ZERO!!!",null, JOptionPane.INFORMATION_MESSAGE);
+         else JOptionPane.showMessageDialog(null, num1+" / "+num2+" = " + quotient(num1, num2),"THE QUOTIENT OF TWO NUMBER",JOptionPane.INFORMATION_MESSAGE);
+
       }
 
       public static double sum(double num1, double num2){
@@ -35,5 +38,4 @@ public class TwoDoubleNumbers {
       public static double quotient(double num1, double num2){
             return num1/num2;
       }
-      
 }
